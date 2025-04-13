3f4440fd9980b78c9be525411e00dadc14357396
Olena Shtronda <e.shtronda@gmail.com>
2025-04-13 14:06:02 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,54 +3,22 @@
 /**
  * Implement method Sort
  */
-/*
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     const arr = this;
     const len = arr.length;
-
-    for (let i = 0; i < len - 1; i++) {
-      for (let j = 0; j < len - 1 - i; j++) {
-        let needsSwap;
-
-        if (compareFunction === undefined) {
-          needsSwap = String(arr[j]) > String(arr[j + 1]);
-        } else {
-          needsSwap = compareFunction(arr[j], arr[j + 1]) > 0;
-        }
-
-        if (needsSwap) {
-          const temp = arr[j];
-
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-        }
-      }
-    }
-
-    return arr;
-  };
-}
-*/
-function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    const arr = this;
-    const len = arr.length;
-    let cf;
+    let compareElements;
 
     if (compareFunction === undefined) {
-      cf = (a, b) => String(a) > String(b);
+      compareElements = (a, b) => String(a) > String(b);
     } else {
-      cf = compareFunction;
+      compareElements = compareFunction;
     }
 
     for (let i = 0; i < len - 1; i++) {
       for (let j = 0; j < len - 1 - i; j++) {
-        if (cf(arr[j], arr[j + 1]) > 0) {
-          const temp = arr[j];
-
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+        if (compareElements(arr[j], arr[j + 1]) > 0) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
       }
     }
